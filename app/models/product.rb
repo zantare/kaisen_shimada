221ab34938b70225cb67f32555b11af57c93fe59
shimada <snoozer.05+zantare@gmail.com>
2010-10-05 15:01:40 +0900

    add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
-  validates_presence_of :name
+  validates_presence_of :name, :description, :image_url
+  validates_numericality_of :price, :greater_than => 0, :only_integer => true
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)\Z}i,
+                      :message => "はGIF,JPEG,PNG画像のURLでなければなりません"
 end
