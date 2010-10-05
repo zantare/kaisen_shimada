b7787747e9078ff6956049e2553954b28cec569b
shimada <snoozer.05+zantare@gmail.com>
2010-10-05 11:04:37 +0900

    rails new kaisen_shimada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaisenShimada::Application.config.secret_token = '8f963716ea6c78c50fd4b48dc588c2c57c608536276f9fee1fabd4659a6eeb4548972b15d9846252fc83f80e1e34c978f3111deaaa65f1fd6aef1e5c2f5d9764'
