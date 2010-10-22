4c459c28a4561c6fd3114d891750fa34f10320d5
shimada <snoozer.05+zantare@gmail.com>
2010-10-22 15:36:23 +0900

    create CartItem

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,16 +1,31 @@
-class Cart
-  def add_product(product)
-    quantities_by_item_id[product.id] += 1
-  end 
+class CartItem
+  attr_reader :product_id, :quantity
 
-  def items_with_quantity
-    quantities_by_item_id.map do |product_id, quantity|
-      [Product.find(product_id), quantity]
-    end
+  def initialize(product)
+    @product_id = product.id
+    @quantity = 1
   end
 
-  private
-  def quantities_by_item_id
-    @quantities_by_item_id ||= Hash.new(0)
+  def increment_quantity
+    @quantity += 1
+  end
+
+  def name
+    Product.find(@product_id).name
+  end
+end
+
+class Cart
+  def add_product(product)
+    current_item = items.find{|item| item.product_id == product.id}
+    if current_item
+      current_item.increment_quantity
+    else
+      items << CartItem.new(product)
+    end
+  end 
+
+  def items
+    @items ||= []
   end
 end
