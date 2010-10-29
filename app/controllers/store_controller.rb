013384e1a39f3f5c09546f5350620d5b215d27e9
shimada <snoozer.05+zantare@gmail.com>
2010-10-29 15:47:26 +0900

    ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,18 +8,27 @@ class StoreController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
     @cart.add_product(@product)
-    redirect_to store_path, :notice => "#{@product.name}が買い物カゴに追加されました"
+    #redirect_to store_path, :notice => "#{@product.name}が買い物カゴに追加されました"
+    respond_to do |format|
+      format.js
+    end
   end
 
   def empty_cart
     @cart.empty!
-    redirect_to store_path, :notice => "買い物カゴは空です"
+    #redirect_to store_path, :notice => "買い物カゴは空です"
+    respond_to do |format|
+      format.js
+    end
   end
 
   def remove_item_from_cart
     product = Product.find(params[:id])
     @cart.remove_product(product)
-    redirect_to store_path, :notice => "#{product.name}を買い物カゴから削除しました"
+    #redirect_to store_path, :notice => "#{product.name}を買い物カゴから削除しました"
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
